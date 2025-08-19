bd07f850ef8c153d6d67a6d5696ee99087b15c79
Dieter Koch <dk@dkoch.org>
2025-08-19 14:47:48 +0200

    Remove ARM platform targets and git tagging from release script

--- image/release-to-dockerhub.sh
+++ image/release-to-dockerhub.sh
@@ -5,16 +5,10 @@ USERNAME=dieterkoch
 # image name
 IMAGE=headscale-caddy-cloudflare-proxy
 # platforms
-PLATFORM=linux/arm64,linux/amd64,linux/arm/v7
+# PLATFORM=linux/arm64,linux/amd64,linux/arm/v7
+PLATFORM=linux/amd64
 # bump version
 version=`awk -F "=" '/TAILSCALE_VERSION=/{print $NF}' Dockerfile`
 echo "Building version: $version"
 # run build
 docker buildx build --platform $PLATFORM -t $USERNAME/$IMAGE:latest -t $USERNAME/$IMAGE:$version --push .
-# tag it
-git add -A
-git commit -m "Tailscale-Caddy-proxy version $version"
-git tag -a "dockerhub-$version" -m "Tailscale-Caddy-proxy version $version"
-git push
-git push --tags
-
